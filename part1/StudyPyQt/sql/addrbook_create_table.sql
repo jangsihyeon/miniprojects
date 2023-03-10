7557f1d70f7cbce0426229ee9a15b6b8386c006a
jangsihyeon <42333610jsh@gmail.com>
2023-03-10 14:08:25 +0900

    테이블 생성 쿼리

---
+++ part1/StudyPyQt/sql/addrbook_create_table.sql
@@ -0,0 +1,10 @@
+-- MySQL 용
+
+CREATE TABLE addressbook (
+  Idx int NOT NULL AUTO_INCREMENT COMMENT 'PK 자동 증가 ',
+  FullName varchar(50) NOT NULL COMMENT '이름 ',
+  PhoneNum varchar(20) NOT NULL COMMENT '핸드폰 번호 ',
+  Email varchar(120) DEFAULT NULL COMMENT '이메일  NULL 허용 ',
+  Address varchar(250)  DEFAULT NULL COMMENT '주소 NULL 허용 ',
+  PRIMARY KEY (Idx)
+)  COMMENT='주소록 테이블 ';
